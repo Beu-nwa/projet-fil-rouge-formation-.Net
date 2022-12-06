087e1d6e1d8e26149b31746157b9134c792e0351
Benoit Combe <benoit.combe59400@gmail.com>
2022-12-06 17:54:39 +0100

    formulaire de connection

--- projet-fil-rouge/src/Components/Component/Form/LogsComp/LogsComp.jsx
+++ projet-fil-rouge/src/Components/Component/Form/LogsComp/LogsComp.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import LoginComp from '../LoginComp/LoginComp';
 import SignInComp from '../SignInComp/SignInComp';
+import './LogsComp.css';
 
 export default class LogsComp extends Component {
     constructor(props) {
@@ -10,10 +11,18 @@ export default class LogsComp extends Component {
         }
     }
     render() {
-        return this.isActive == true ? (
-            <LoginComp isActive={this.isActive} />
+        return this.state.isActive ? (
+            <div className='formContainer row justify-content-center'>
+                <div className='col col-3 my-4'>
+                    <SignInComp isActive={this.state.isActive} />
+                </div>
+            </div>
         ) : (
-            <SignInComp />
+            <div className='formContainer row justify-content-center'>
+                <div className='col col-3 my-4'>
+                    <LoginComp />
+                </div>
+            </div >
         )
     }
 }
--- projet-fil-rouge/src/Components/Component/Form/SignInComp/SignInComp.jsx
+++ projet-fil-rouge/src/Components/Component/Form/SignInComp/SignInComp.jsx
@@ -6,7 +6,27 @@ export default class SignInComp extends Component {
     }
     render() {
         return (
-            <div>SignInComp</div>
+            <div className='card p-2'>
+                <label className='input-group row my-2 d-flex align-items-center'>
+                    <div className="col-5 d-flex align-items-start">Email :</div>
+                    <input className='col-7 input-group-text' type="text" placeholder='Email...' />
+                </label>
+                <label className='input-group row my-2 d-flex align-items-center'>
+                    <div className="col-5 d-flex align-items-start">Mot de passe :</div>
+                    <input className='col-7 input-group-text' type="text" placeholder='Mot de passe...' />
+                </label>
+                <hr />
+                <div className='d-flex align-items-center justify-content-around mb-3'>
+                    <div className=''>
+                        <span>
+                            créer un compte...
+                        </span>
+                    </div>
+                    <div className=''>
+                        <button className='btn btn-outline-dark'>se connecter</button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
