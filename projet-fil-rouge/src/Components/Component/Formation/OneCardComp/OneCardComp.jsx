a01d87ce8a59626fbe3f1d6034db06b9393f8e76
Maxime Letellier <maximeletellier08@gmail.com>
2022-12-07 00:15:57 +0100

    style onecard

--- projet-fil-rouge/src/Components/Component/Formation/OneCardComp/OneCardComp.jsx
+++ projet-fil-rouge/src/Components/Component/Formation/OneCardComp/OneCardComp.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from 'react';
-import './OneCardComp.css'
+import './OneCardComp.css';
+import {StarFill} from 'react-bootstrap-icons';
+import {StarHalf} from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
+ 
+
+//TODO :::  Incrémenter prix date etc.. Avec BDD//
 
 class OneCardComp extends Component {
     render() {
         return (
             <div className="card">
-                <div className="card-img"></div>
-                <div className="card-info">
-                    <svg height="24" width="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M0 0h24v24H0z" fill="none"></path><path d="M4.828 21l-.02.02-.021-.02H2.992A.993.993 0 0 1 2 20.007V3.993A1 1 0 0 1 2.992 3h18.016c.548 0 .992.445.992.993v16.014a1 1 0 0 1-.992.993H4.828zM20 15V5H4v14L14 9l6 6zm0 2.828l-6-6L6.828 19H20v-1.172zM8 11a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" fill="currentColor"></path></svg>
-                    <p className="text-title">EDEDEDEDE</p>
-                    <p className="text-body">Lorem Ipsum dolor sit amet</p>
-                    <button className="card-button">En savoir plus</button>
-                    <button className="card-button"></button>
-                </div>
-            </div>
+<div className="card-image"> <StarFill color='yellow'/> <StarFill color='yellow' />  <StarFill color='yellow'/> <StarFill color='yellow'/> <StarHalf color='yellow'/>  <hr/>
+    <p className='infoForma'>Développez vos compétences techniques en C# React et Docker ainsi que vos softskills <hr/>Prix : 2500€<hr/>Durée: 6 mois<hr/>Prochaine session: 03/02/2023</p></div>
+  <div className="card-description">
+    <p className="text-title">Ma formation
+    </p>
+    <button className='btn-post'>Postuler</button>
+    <button className='btn-cart'>Ajouter au panier</button>
+    <p className="text-body">Blabla ma formation de 6mois pour faire fullstack gnagnagna</p>
+  </div>
+</div>
         );
     }
 }
--- projet-fil-rouge/src/Views/FormationView/FormationView.jsx
+++ projet-fil-rouge/src/Views/FormationView/FormationView.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import CardsComp from '../../Components/Component/Formation/CardsComp/CardsComp';
 import OneCardComp from '../../Components/Component/Formation/OneCardComp/OneCardComp';
 
 class FormationView extends Component {
     render() {
         return (
             <div>
-                <OneCardComp />
+                <CardsComp />
             </div>
         );
     }
