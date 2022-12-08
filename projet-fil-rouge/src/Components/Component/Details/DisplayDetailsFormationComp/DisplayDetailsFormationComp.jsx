b7d15b416e4ef5db5ef46ff0107aece5bf6d63d3
Maxime Letellier <maximeletellier08@gmail.com>
2022-12-08 13:23:42 +0100

    stars

---
+++ projet-fil-rouge/src/Components/Component/Details/DisplayDetailsFormationComp/DisplayDetailsFormationComp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class DisplayDetailsFormationComp extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default DisplayDetailsFormationComp;
