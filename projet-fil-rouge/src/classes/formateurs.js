e4a452a22ec740cdbb90f961311bab3895451108
Benoit Combe <benoit.combe59400@gmail.com>
2022-12-08 14:40:45 +0100

    api et apiServices

--- projet-fil-rouge/src/classes/formateurs.js
+++
@@ -1,8 +0,0 @@
-import users from "./users.js";
-
-export default class formateurs extends users {
-    constructor(nom, prenom, mail, mpd, status){
-        super(nom, prenom, mail, mpd, status);
-        this.status = "formateur";
-    }
-}
--- projet-fil-rouge/src/classes/stagiaire.js
+++
@@ -1,8 +0,0 @@
-import users from "./users.js";
-
-export default class stagiaires extends users {
-    constructor(nom, prenom, mail, mpd, status){
-        super(nom, prenom, mail, mpd, status);
-        this.status = "stagiaire";
-    }
-}
